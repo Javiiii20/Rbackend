062a2d53e2f3f9cc707f02423b9d61daeb304411
Javiiii20 <ordoezjavier23@gmail.com>
2024-05-10 17:16:23 -0600

    Cambios iniciales

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/api/citas', (req, res) => {
 app.get('/api/citas/disponibilidad', (req, res) => {
   const { fecha, hora } = req.query;
 
-  const query = 'SELECT * FROM citas';
+  const query = 'SELECT * FROM citas WHERE fecha = $1 AND hora = $2';
   const values = [fecha, hora];
 
   pool.query(query, values, (err, result) => {
@@ -65,7 +65,6 @@ app.get('/api/citas/disponibilidad', (req, res) => {
     } else {
       // No hay citas disponibles
       res.status(200).json({ disponible: false });
-      res.status(200).json(result.rows);
     }
   });
 });
